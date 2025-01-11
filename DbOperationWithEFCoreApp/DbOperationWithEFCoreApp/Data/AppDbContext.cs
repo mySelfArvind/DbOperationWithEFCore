0609c4697014a5301da417350401b9bba5f47b7d
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2025-01-11 20:54:04 +0530

    Added Entities to Db

--- DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Data/AppDbContext.cs
+++ DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Data/AppDbContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DbOperationWithEFCoreApp.Entities;
+using Microsoft.EntityFrameworkCore;
 namespace DbOperationWithEFCoreApp.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions options):base(options) { }
+
+        public DbSet<Language> Language { get; set; }
+        public DbSet<CurrencyType> CurrencyType { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Price> Price { get; set; }
     }
 }
