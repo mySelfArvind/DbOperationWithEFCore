5690b859ff774115bcd5fc1f4a5349ee1abb23d7
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2025-01-11 20:37:00 +0530

    Installed EFCore packages, Setup DbContext

---
+++ DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Data/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DbOperationWithEFCoreApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options) { }
+    }
+}
