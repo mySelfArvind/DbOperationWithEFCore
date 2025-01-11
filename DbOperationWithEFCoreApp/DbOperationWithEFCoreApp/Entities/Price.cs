3d624310fb35e12a80f2fed4bcdfa44cc71246e9
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2025-01-11 20:58:33 +0530

    Updated CurrentId to CurrentTypeId

--- DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Entities/Price.cs
+++ DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Entities/Price.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int BookId { get; set; }
         public double Amount { get; set; }
-        public int CurrencyId { get; set; }
+        public int CurrencyTypeId { get; set; }
 
         public Book Book { get; set; }
         public CurrencyType CurrencyType { get; set; }
