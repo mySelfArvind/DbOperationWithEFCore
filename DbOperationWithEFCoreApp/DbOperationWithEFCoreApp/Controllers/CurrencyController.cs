bd431665ca87f5ab89bd6b60891f4cbdf0f8f2a9
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2025-01-12 23:25:12 +0530

    Added Two API Controller

---
+++ DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Controllers/CurrencyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DbOperationWithEFCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+namespace DbOperationWithEFCoreApp.Controllers
+{
+    [Route("api/currencies")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CurrencyController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCurrencies()
+        {
+            //var result = await _appDbContext.CurrencyType.ToListAsync();
+            var result = await (from currencies in _appDbContext.CurrencyType
+                                select new { currencies.Id, currencies.Title, currencies.Description }).ToListAsync();
+            return Ok(result);
+        }
+    }
+}
---
+++ DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Controllers/LanguageController.cs
@@ -0,0 +1,24 @@
+﻿using DbOperationWithEFCoreApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationWithEFCoreApp.Controllers
+{
+    [Route("api/languages")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public LanguageController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllLanguages()
+        {
+            return Ok(await _appDbContext.Language.ToListAsync());
+        }
+    }
+}
