3daf5b84794efd99803c194ce4dd7befe2c9182c
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2025-01-18 22:21:32 +0530

    added few more get method to get single currency in different ways.

--- DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Controllers/CurrencyController.cs
+++ DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Controllers/CurrencyController.cs
@@ -33,14 +33,69 @@ namespace DbOperationWithEFCoreApp.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// get unique currency by using title, and we're assuming that currency are not duplicate...
+        /// if given title found duplicate currency then we'll get error[exception]...
+        /// </summary>
+        /// <param name="title"></param>
+        /// <returns></returns>
         [HttpGet("{title}")]
         public async Task<IActionResult> GetCurrencyByTitle([FromRoute] string title)
         {
             //var result = await _appDbContext.CurrencyType.Where(c => c.Title == title).FirstOrDefaultAsync();
             var result = await _appDbContext.CurrencyType.SingleOrDefaultAsync(c => c.Title == title);
-            if(result == null)
+            if (result == null)
                 return NotFound();
             return Ok(result);
         }
+
+        /// <summary>
+        /// get currency based on title, if more than one record available with same title then first will be considered
+        /// </summary>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        //[HttpGet("{title}")]
+        //public async Task<IActionResult> GetCurrencyByTitleAsync([FromRoute] string title)
+        //{
+        //    var result = await _appDbContext.CurrencyType.FirstOrDefaultAsync(c => c.Title == title);
+        //    if (result == null)
+        //        return NotFound();
+        //    return Ok(result);
+        //}
+
+        /// <summary>
+        /// get currency by passing title and description where both are part of Route and mandatory...
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        //[HttpGet("{title}/{description}")]
+        //public async Task<IActionResult> GetCurrencyByTitleAndDescriptionAsync([FromRoute] string title, [FromRoute] string description)
+        //{
+        //    var result = await _appDbContext.CurrencyType.FirstOrDefaultAsync(c => c.Title == title && c.Description == description);
+        //    if (result == null)
+        //        return NotFound();
+        //    return Ok(result);
+        //}
+
+        /// <summary>
+        /// get currency by passing title[FromRoute] and description[FromQuery] where description is optioanl 
+        /// if passed then will be used else will be ommited...
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        //[HttpGet("{title}")]
+        //public async Task<IActionResult> GetCurrencyByTitleAndDescriptionAsync([FromRoute] string title, [FromQuery] string? description)
+        //{
+        //    var result = await _appDbContext.
+        //        CurrencyType.
+        //        FirstOrDefaultAsync(c => 
+        //        c.Title == title &&
+        //        (string.IsNullOrEmpty(description) || c.Description == description));
+        //    if (result == null)
+        //        return NotFound();
+        //    return Ok(result);
+        //}
     }
 }
